69945235710a2af3b390a36212ed527e5fb24338
Atul Sharma <asharma@sw-at.com>
2020-06-03 20:09:43 +0100

    remove comments

--- src/001_basic_sketch/src/main.cpp
+++ src/001_basic_sketch/src/main.cpp
@@ -1,78 +1,62 @@
 #include <Arduino.h>
 
-
 int initialize(int, int);
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   Serial.println("Initalizing...");
   int val = initialize(1, 3);
   Serial.println("Initialized with : " + String(val));
 
+  Serial.println("--- Variable type: Size in SRAM (bytes) ---");
 
-   Serial.println("--- Variable type: Size in SRAM (bytes) ---");
+  Serial.print("uint8_t: ");
+  Serial.println(sizeof(uint8_t));
 
+  Serial.print("boolean: ");
+  Serial.println(sizeof(boolean));
 
- // C standard data type "unsigned int length 8 bits". 
- // Occupies one byte of memory.
- // http://arduino.cc/forum/index.php/topic,41590.0.html
- // http://en.wikipedia.org/wiki/Stdint.h#Fixed_width_integer_types
- Serial.print("uint8_t: ");
- Serial.println(sizeof(uint8_t));
+  Serial.print("char: ");
+  Serial.println(sizeof(char));
 
- // A boolean holds one of two values, true or false. 
- // Each boolean variable occupies one byte of memory.
- // http://arduino.cc/en/Reference/BooleanVariables
- Serial.print("boolean: ");
- Serial.println(sizeof(boolean));
+  Serial.print("unsigned char: ");
+  Serial.println(sizeof(unsigned char));
 
+  Serial.print("byte: ");
+  Serial.println(sizeof(byte));
 
- Serial.print("char: ");
- Serial.println(sizeof(char));
+  Serial.print("int: ");
+  Serial.println(sizeof(int));
 
- 
- Serial.print("unsigned char: ");
- Serial.println(sizeof(unsigned char));
- 
+  Serial.print("unsigned int: ");
+  Serial.println(sizeof(unsigned int));
 
- Serial.print("byte: ");
- Serial.println(sizeof(byte));
- 
+  Serial.print("word: ");
+  Serial.println(sizeof(word));
 
- Serial.print("int: ");
- Serial.println(sizeof(int));
+  Serial.print("long: ");
+  Serial.println(sizeof(long));
 
+  Serial.print("unsigned long: ");
+  Serial.println(sizeof(unsigned long));
 
- Serial.print("unsigned int: ");
- Serial.println(sizeof(unsigned int));
+  Serial.print("float: ");
+  Serial.println(sizeof(float));
 
-
- Serial.print("word: ");
- Serial.println(sizeof(word));
- 
-
- Serial.print("long: ");
- Serial.println(sizeof(long));
-
-
- Serial.print("unsigned long: ");
- Serial.println(sizeof(unsigned long));
-
-
- Serial.print("float: ");
- Serial.println(sizeof(float));
- 
-
- Serial.print("double: ");
- Serial.println(sizeof(double));
+  Serial.print("double: ");
+  Serial.println(sizeof(double));
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 }
 
-int initialize(int a, int b){
-  Serial.println("Parameter a: " + String(a)); 
-  Serial.println( "Parameter b: " + String(b)); 
+int initialize(int a, int b)
+{
+  Serial.println("Parameter a: " + String(a));
+  Serial.println("Parameter b: " + String(b));
   Serial.println("Initialized");
-  return a + b;}
+  return a + b;
+}
